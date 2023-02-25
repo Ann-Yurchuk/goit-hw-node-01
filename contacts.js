1fe60c7dd727617a1679f7c64b7b19343e3c23d6
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2023-02-25 18:34:00 +0200

    first commit

---
+++ contacts.js
@@ -0,0 +1,63 @@
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
+
+const contactsPath = path.resolve(__dirname, "..", "db", "contacts.json");
+
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log("====================================");
+    console.log(error.message);
+    console.log("====================================");
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    return contacts.find((contact) => String(contact.id) === String(contactId));
+  } catch (error) {
+    console.log("====================================");
+    console.log(error.message);
+    console.log("====================================");
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const newContactsList = contacts.filter(
+      (contact) => String(contact.id) !== String(contactId)
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(newContactsList, null, 4));
+  } catch (error) {
+    console.log("====================================");
+    console.log(error.message);
+    console.log("====================================");
+  }
+};
+
+const addContact = async (name, email, phone) => {
+  try {
+    const id = crypto.randomUUID();
+    const contacts = await listContacts();
+    const newContact = { id, name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(newContact, null, 4));
+    return newContact;
+  } catch (error) {
+    console.log("====================================");
+    console.log(error.message);
+    console.log("====================================");
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
